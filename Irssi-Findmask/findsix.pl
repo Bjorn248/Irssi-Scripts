7152c1c1b1ebd6e533c57875d5f1f3f365fecd52
Bjorn Stange <bjorn248@gmail.com>
2011-11-29 05:00:59 -0500

    version 4

--- Irssi-Findmask/findsix.pl
+++ Irssi-Findmask/findsix.pl
@@ -24,7 +24,7 @@ sub check_six {
 	my @out = @_;
 	my @who = split(/\s+/, $out[1]);
 	my $mask = $who[3];
-	if ($mask =~ m/98brov/ || $mask =~ m/qimmtt/ || $who[2] =~ m/james/i) {
+	if ($mask =~ m/98brov/ || $mask =~ m/qimmtt/ || $who[2] =~ m/james/i || $who[2] =~ m/sixmsj/i) {
 		push(@six, $who[5]);
 	}
 }
